searchlight/go-jmap

commit 821d0924a12e627350a2e662210f2e08e26e271e

dates: force UTC timezone
In the RFC, it is stated that all (or almost all) dates should be
"UTCDate" (e.g. in the UTC timezone and formatted with a trailing "Z").

Convert the dates to use the UTC timezone when required before
marshaling to JSON. Create type aliases to be able to call
json.Marshal() from MarshalJSON() without a recursion error.

Link: https://www.rfc-editor.org/rfc/rfc8620.html#section-1.4
Link: https://www.rfc-editor.org/rfc/rfc8621.html#section-4.4.1
Link: https://choly.ca/post/go-json-marshalling/
Signed-off-by: Robin Jarry <robin@jarry.cc>

mail/vacationresponse/vacationresponse.go
@@ -1,6 +1,7 @@
 package vacationresponse
 
 import (
+	"encoding/json"
 	"time"
 
 	"git.sr.ht/~rockorager/go-jmap"
@@ -48,4 +49,16 @@ type VacationResponse struct {
 
 	// The HTML body to send in the response
 	HTMLBody *string `json:"htmlBody,omitempty"`
+}
+
+func (v *VacationResponse) MarshalJson() ([]byte, error) {
+	if v.FromDate != nil && v.FromDate.Location() != time.UTC {
+		utc := v.FromDate.UTC()
+		v.FromDate = &utc
+	}
+	if v.ToDate != nil && v.ToDate.Location() != time.UTC {
+		utc := v.ToDate.UTC()
+		v.ToDate = &utc
+	}
+	return json.Marshal(v)
 }

mail/email/filter.go
@@ -1,6 +1,7 @@
 package email
 
 import (
+	"encoding/json"
 	"time"
 
 	"git.sr.ht/~rockorager/go-jmap"
@@ -127,3 +128,17 @@ type FilterCondition struct {
 }
 
 func (fc *FilterCondition) implementsFilter() {}
+
+func (fc *FilterCondition) MarshalJSON() ([]byte, error) {
+	if fc.Before != nil && fc.Before.Location() != time.UTC {
+		utc := fc.Before.UTC()
+		fc.Before = &utc
+	}
+	if fc.After != nil && fc.After.Location() != time.UTC {
+		utc := fc.After.UTC()
+		fc.After = &utc
+	}
+	// create a type alias to avoid infinite recursion
+	type Alias FilterCondition
+	return json.Marshal((*Alias)(fc))
+}

mail/emailsubmission/emailsubmission.go
@@ -89,6 +89,16 @@ type EmailSubmission struct {
 	MDNBlobIDs []jmap.ID `json:"mdnBlobIds,omitempty"`
 }
 
+func (s *EmailSubmission) MarshalJSON() ([]byte, error) {
+	if s.SendAt != nil && s.SendAt.Location() != time.UTC {
+		utc := s.SendAt.UTC()
+		s.SendAt = &utc
+	}
+	// create a type alias to avoid infinite recursion
+	type Alias EmailSubmission
+	return json.Marshal((*Alias)(s))
+}
+
 type Envelope struct {
 	// The email address to use as the return address in the SMTP submission
 	MailFrom *Address `json:"mailfrom,omitempty"`

mail/emailsubmission/filter.go
@@ -1,6 +1,7 @@
 package emailsubmission
 
 import (
+	"encoding/json"
 	"time"
 
 	"git.sr.ht/~rockorager/go-jmap"
@@ -48,3 +49,17 @@ type FilterCondition struct {
 }
 
 func (fc *FilterCondition) implementsFilter() {}
+
+func (fc *FilterCondition) MarshalJSON() ([]byte, error) {
+	if fc.Before != nil && fc.Before.Location() != time.UTC {
+		utc := fc.Before.UTC()
+		fc.Before = &utc
+	}
+	if fc.After != nil && fc.After.Location() != time.UTC {
+		utc := fc.After.UTC()
+		fc.After = &utc
+	}
+	// create a type alias to avoid infinite recursion
+	type Alias FilterCondition
+	return json.Marshal((*Alias)(fc))
+}

commit 139a2611617355d1fcf875de6eb61a60ec8671f2

vacationresponse: fix infinite recursion
Fix infinite recursion when marshaling a VacationResponse object to
JSON. This type alias was forgotten.

Fixes: 821d0924a12e ("dates: force UTC timezone")
Signed-off-by: Robin Jarry <robin@jarry.cc>

mail/vacationresponse/vacationresponse.go
@@ -60,5 +60,6 @@ func (v *VacationResponse) MarshalJson() ([]byte, error) {
 		utc := v.ToDate.UTC()
 		v.ToDate = &utc
 	}
-	return json.Marshal(v)
+	type Alias VacationResponse
+	return json.Marshal((*Alias)(v))
 }